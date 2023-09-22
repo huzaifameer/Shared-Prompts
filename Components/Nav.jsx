773a14f7800b9940a77224f865c06d615437e8e0
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-22 21:42:31 +0530

    commit13

--- Components/Nav.jsx
+++ Components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from 'react';
 import {signIn,signOut,useSession,getProviders} from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn=true;
+  const {data:session}=useSession();
   const [providers,setProviders] = useState(null);
-  const[toggleDropDown,setToggleDropDown]=useState(false);
+  const[toggleDropDown,setToggleDropDown] = useState(false);
 
   useEffect(()=>{
-    const setProviders=async ()=>{
+    const setUpProviders=async () =>{
       const response=await getProviders();
 
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   },[])
   
   return (
@@ -26,9 +26,10 @@ const Nav = () => {
         <Image src="/assets/images/logo-solyd.png" alt='SollydZAi-Logo' width={60} height={50} className='object-contain' />
         <p className='logo_text'>SollydZAi</p>
       </Link>
+      
       {/*Desktop Navigation*/}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ?(
+        {session?.user ?(
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt" className='black_btn'>Create Post</Link>
             <button type='button' onClick={signOut} className='outline_btn'>Sign Out</button>
@@ -50,7 +51,7 @@ const Nav = () => {
       </div>
       {/*mobile navigation*/}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn?(
+        {session?.user ?(
           <div className='flex'>
             <Image src="assets/images/logo.svg" width={37} height={37} className='rounded-full' alt='profile' 
             onClick={()=>setToggleDropDown((prev)=>!prev)}/>
